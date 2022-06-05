f065cc3949f23cd7e7acbc6073561cb379210894
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-05 12:44:05 -0500

    Create 1688.java

---
+++ Easy/June/1688.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int result = 0;        
+        while(n>=2){   
+            if(n%2==0){
+                result+=n/2;
+                n/=2;
+            }
+            else{
+                result+=(n-1)/2;
+                n = (n-1)/2 +1;
+            }
+                    
+        }
+        
+        return result;
+        
+    }
+}
