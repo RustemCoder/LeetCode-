119cfc438d3d932adf69e65a38c6acb91635fe1a
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-06-06 19:24:13 -0500

    Create 304.java

---
+++ medium/june/304.java
@@ -0,0 +1,29 @@
+class NumMatrix {
+    private int[][] dp;
+    public NumMatrix(int[][] matrix) {
+		dp = new int[matrix.length][matrix[0].length];
+		populateArray(matrix, dp);
+	}
+
+	private void populateArray(int[][] arr, int[][] dp) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				if (j == 0) {
+					dp[i][j] = arr[i][j];
+				} else {
+					dp[i][j] = arr[i][j] + dp[i][j - 1];
+				}
+			}
+		}
+	}
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        
+		int sum = 0;
+		for (int i = row1; i <= row2; i++) {
+			sum = sum + (dp[i][col2] - (col1 > 0 ? dp[i][col1 - 1] : 0));
+		}
+		return sum;
+	
+    }
+}
+
